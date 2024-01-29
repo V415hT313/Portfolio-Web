b849144c9ef195818cc9e05821a9f640c8fafcb2
V415hT313 <vashu2506rkt@gmail.com>
2024-01-29 14:26:29 +0530

    first commit

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,120 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>MERN</li>
+        <li>NEXT.JS</li>
+        <li>JavaScript</li>
+        <li>Git</li>
+        <li>C++ & Python</li>
+        <li>Cloud Computing</li>
+        <li>Data Science & ML</li>
+        
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Bachelor of Technology(CSE)</li>
+        <li>Galgotias University, Greater Noida</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>SIH(Smart India Hackathon) -2019</li>
+        <li>Dextrix 2.0 - 2019</li>
+        <li>Ethical Hacking Workshop -2020(at IIT Roorkee)</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Badges",
+    id: "badges",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Generative AI Fundamentals</li>
+        <li>Introduction to LLM</li>
+        <li>Introduction to Responsible AI</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am a full stack web developer with a passion for creating
+            interactive and responsive web applications. I have experience
+            working with JavaScript, React, Redux, Node.js, Express, HTML, CSS, and Git.
+            I am a quick learner and I am always
+            looking to expand my knowledge and skill set. I am a team player and
+            I am excited to work with others to create amazing applications.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("badges")}
+              active={tab === "badges"}
+            >
+              {" "}
+              Badges{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/app/components/ProjectsSection.jsx
@@ -0,0 +1,133 @@
+"use client";
+import React, { useState, useRef } from 'react';
+import ProjectCard from './ProjectCard';
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
+
+const projectsData = [
+    {
+      id: 1,
+      title: "Social Media Application",
+      image: "/images/projects/1.jpg",
+      tag: ["All", "Web"],
+      gitUrl: "https://github.com/V415hT313/ReactSocialMedia",
+      previewUrl: "https://react-social-media-sable.vercel.app/",
+    },
+    {
+      id: 2,
+      title: "E-commerce Website",
+      image: "/images/projects/2.jpg",
+      tag: ["All", "Web"],
+      gitUrl: "https://github.com/V415hT313/ecommerce",
+      previewUrl: "/",
+    },
+    {
+      id: 3,
+      title: "Dalle-Clone",
+      image: "/images/projects/3.png",
+      tag: ["All", "Web"],
+      gitUrl: "https://github.com/V415hT313/Dalle-Clone",
+      previewUrl: "/",
+    },
+   
+    {
+      id: 4,
+      title: "Olympics-Survey",
+      image: "/images/projects/4.jpg",
+      tag: ["All", "ML"],
+      gitUrl: "https://github.com/V415hT313/OlympicsMain",
+      previewUrl: "https://olympicsmain-gvz57fgjzrivyey2hp7e5i.streamlit.app/",
+    },
+    {
+      id: 5,
+      title: "Potato-Disease Classification",
+      image: "/images/projects/5.jpg",
+      tag: ["All", "ML"],
+      gitUrl: "https://github.com/V415hT313/DeepLearningProject",
+      previewUrl: "/",
+    },
+    {
+        id: 6,
+        title: "Car Price Prediction",
+        image: "/images/projects/6.jpg",
+        tag: ["All", "ML"],
+        gitUrl: "/",
+        previewUrl: "https://github.com/V415hT313/CarPRice",
+    },
+    {
+        id: 7,
+        title: "Water Quality Prediction",
+        image: "/images/projects/7.jpg",
+        tag: ["All", "ML"],
+        gitUrl: "https://github.com/V415hT313/Quality_Prediction",
+        previewUrl: "/",
+    },
+];
+
+const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) => 
+    project.tag.includes(tag)
+  );
+  
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+    
+  return (
+    <section>
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+      My Projects
+      </h2>
+
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="ML"
+          isSelected={tag === "ML"}
+        />
+      </div>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+
+      </ul>
+                    
+    </section>
+  );
+};
+
+export default ProjectsSection;
