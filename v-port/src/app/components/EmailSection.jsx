59a044c008132f3fa45f3413c8b2b177005e3486
Vaishnavi Tripathi <60942298+V415hT313@users.noreply.github.com>
2024-01-31 23:05:58 +0530

    Update EmailSection

--- v-port/src/app/components/EmailSection.jsx
+++ v-port/src/app/components/EmailSection.jsx
@@ -56,10 +56,10 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
+          <Link href="https://github.com/V415hT313">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
-          <Link href="linkedin.com">
+          <Link href="https://www.linkedin.com/in/vaishnavi-tripathi-3a82731a2/">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
